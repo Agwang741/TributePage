8d78225c119fbfa7d9bf81ebc3a87d8870e9fda0
Agwang741 <71236871+Agwang741@users.noreply.github.com>
2020-09-13 20:12:09 -0400

    Initial Version

---
+++ maejamison.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<title>Tribute to Dr. Mae Jamison</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://birchtreedev.com/supportfiles/btd-style.css">
+<link rel="stylesheet" href="tribute-style.css">
+<body>
+
+<!----------------------------------------------------------->
+<!-- This section is for the header                        -->
+<?php readfile("https://birchtreedev.com/includes/btd-nav.php"); ?>
+<!----------------------------------------------------------->
+
+<dev class="btd-content btd-padding" style="max-width:1564px">
+<!-- Page content -->
+<main id="main">
+
+
+<h1 class="btd-padding-24 btd-center" id="title">Dr. Mae C. Jamison</h1>
+<h3 class="btd-center">NASA Astronaut, Physician, and Engineer </h3>
+
+<figure id="img-div">
+    <img id="img-div" src="https://birchtreedev.com/projects/01TributePage/2048px-Mae_Carol_Jemison.jpg" alt="Dr. Mae Jamison">
+    <figcaption id="img-caption">
+    Dr. Jamison joined NASA in 1987 and served on the STS-47 mission in 1992 where she orbited the Earth for 8 days.
+    <figcaption>
+</figure>
+
+<section id="tribute-info">
+    <h3 id="headline"> Here are the highlights of Dr. Jamison's life:</h3>
+    <ul>
+        <li><strong>1956</strong> - Born in Decatur, Alabama</li>
+        <li><strong>1973</strong> - Graduated from high school and entered Stanford University at age 16</li>
+        <li><strong>1977</strong> - Graduated from Stanford University with a degree in Chemical Engineering</li>
+        <li><strong>1981</strong> - Graduated from Cornell Medical School with an MD </li>
+        <li><strong>1982</strong> - Worked for Ross–Loos Medical Group as a general practitioner</li>
+        <li><strong>1983</strong> - Worked for the Peace Corp in  Sierra Leone and Liberia. She was responsible for the volunteer's medical care and writing self-care manuals. In addition, she supervised the Peace Corp's pharmacy, laboratory, medical staff, as well as health and safety issues.</li>
+        <li><strong>1987</strong> - Applied for the NASA astronaut program and was chosen out of 2000 applicants.</li>
+        <li><strong>1992</strong> - Flew on the STS-47 mission where she orbited the Earth 127 times. Her experiments include:
+            <ul>
+                <li>Testing the Autogenic Feedback Training Exercise (AFTE) on herself and Japanese astronaut Mamoru Mohri. AFTE uses biofeedback to help patients control their motion sickness, anxiety, etc.</li>
+                <li>Testing the NASA's Fluid Therapy System where she followed a set of procedures to produce water for injection.</li>
+                <li>Following a second set of procedures to use the water from the Fluid Therapy System to create a saline solution.</li>
+                <li>Co-investigating two bone cell experiments.</li>
+                <li>Inducing frogs to ovulate, fertilized the eggs, and observed the behavior of the tadpoles in a zero-g environment.</li>
+            </ul></li>
+        <li><strong>1993</strong> - Founded The Jemison Group Inc. after leaving NASA. Her company evaluates the social and cultural impacts of technology and design.</li>
+        <li><strong>1994</strong> - Founded the Dorothy Jemison Foundation for Excellence which is named after her mother.  This company produces summer camps for kids 12 - 16 years old.</li>
+        <li><strong>1995 to 2002</strong> - She taught as a professor of Environmental Studies at Dartmouth College.</li>
+        <li><strong>1999</strong> - Taught as an Andrew D. White Professor-at-Large at Cornell University.  As a professor-at-large, she visited Cornell University for at least one week to "enliven the intellectual and cultural life of the university".</li>
+        <li><strong>1999</strong> - Founded BioSentient Corp and obtained a license to commercialize AFTE.  This is the process she and tested on themselves during the STS-47 space flight.</li>
+        <li><strong>2012</strong> - Submitted the winning bid for a (DARPA) 100 Year Starship Project through the Dorothy Jemison Foundation for Excellence. The goal is to create a business plan to foster research for interstellar flight within 100 years. </li>
+        <li><strong>2018</strong> - Collaborated in an initiative with  Bayer Crop Science and National 4-H Council.  Their goal is to encourage agricultural sciences for young children.</li>
+    </ul>
+    <blockquote>
+        
+    </blockquote>
+    <h3>If you would like to learn more, take a look at this amazing human being on <a id="tribute-link" href="https://en.wikipedia.org/wiki/Mae_Jemison" target="_blank">Wikipedia</a>.</h3>
+</section>
+
+
+
+
+
+</main>
+<!-- End page content -->
+</dev>
+
+
+<!-- Footer -->
+<!----------------------------------------------------------->
+<!-- This section is for the footer                        -->
+<?php readfile("https://birchtreedev.com/includes/btd-footer.php"); ?>
+<!----------------------------------------------------------->
+
+
+</body>
+</html>
